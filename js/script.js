3b9607b014eb0ec2e4f0f85809a40f052a1d95cb
Danny Witting <mindpaste@gmx.de>
2012-03-03 23:00:54 +0100

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 $.fn.ready( function() {
 
-	$.q("textarea").on("focus", function(){
-		
-		$("textarea").tabby();
-	};
-	
 	$.q("pre,code").not(".nopaste").each(function(){
 		
 		$(this).text($(this).html());
